757194d7b8199aa831294459af2fe27c42d7f6f4
laith <laithalwani@gmail.com>
2024-12-27 16:06:42 -0500

    simple ui fix

--- app/page.js
+++ app/page.js
@@ -1,31 +1,27 @@
-'use client'
-import { useChat } from "ai/react"
+"use client";
+import { useChat } from "ai/react";
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-          {messages.map((m) => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === "user" ? "User: " : "AI: "}
-              {m.content}
-            </div>
-          ))}
+    <main className="min-h-screen flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <div className="flex flex-col w-full h-full max-w-md py-24 mx-auto stretch">
+        {messages.map((m) => (
+          <div key={m.id} className="whitespace-pre-wrap">
+            {m.role === "user" ? "User: " : "AI: "}
+            {m.content}
+          </div>
+        ))}
 
-          <form onSubmit={handleSubmit}>
-            <input
-              className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-              value={input}
-              placeholder="Say something..."
-              onChange={handleInputChange}
-            />
-          </form>
-        </div>
-      </main>
-
-    </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
+    </main>
   );
 }
- 
