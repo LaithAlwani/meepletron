d4f6200fb2aa14a49d696066f2e877add3654a35
laith <laithalwani@gmail.com>
2024-12-29 20:32:17 -0500

    adds gradient text for app name and adds text to bot bubble

--- app/page.js
+++ app/page.js
@@ -3,13 +3,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="text-center pt-10 relative">
+      <div className="text-center pt-32 relative">
         <div className="relative w-[20rem] h-[20rem] mx-auto left-5">
-          <Image src="/chatbot.png" fill priority alt="robot logo" />
+          <Image src="/chatbot-ed.png" fill priority alt="robot logo" />
+          <span className="absolute text-left font-bold text-[10px] top-[2.1rem] right-[1.6rem] bg-gradient-to-tr from-purple-500 to-red-500 text-transparent bg-clip-text">
+            Hi, I'm Jenna!
+            <br />
+            The Rules Guru. 
+            <br/>Ready to Help!
+          </span>
         </div>
-        
-        <h1 className="text-2xl uppercase font-bold pt-5">
-          Welcome to Rules Guru
+
+        <h1 className="text-2xl uppercase font-bold pt-5 bg-gradient-to-br from-purple-500 to-red-500 text-transparent bg-clip-text">
+          Rules Guru
         </h1>
         <p className="italic up text-sm pb-5">Say goodbye to rulebook flipping!</p>
         <Link
