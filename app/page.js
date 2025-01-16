69012197815afbdbbf1b70d4a43bd49bf614bdeb
laith <laithalwani@gmail.com>
2025-01-16 16:01:05 -0500

    changes text

--- app/page.js
+++ app/page.js
@@ -9,47 +9,23 @@ export default function Home() {
   const { user } = useUser();
   return (
     <>
-      {/* <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Board Game Wizard</h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <a href="#features" className="hover:underline">
-                Features
-              </a>
-            </li>
-            <li>
-              <a href="#how-it-works" className="hover:underline">
-                How It Works
-              </a>
-            </li>
-            <li>
-              <a href="#pricing" className="hover:underline">
-                Pricing
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div> */}
       <section className="text-center relative max-w-lg mx-auto px-3">
         <div className="relative max-w-[18rem] sm:w-[20rem] h-[18rem] sm:h-[20rem] mx-auto left-5 my-4">
           <Image src="/chatbot-ed.png" fill priority alt="robot logo" />
           <span
             className={`absolute text-left font-bold ${
               user
-                ? "text-[18px] top-[2.4rem] sm:top-[2.8rem]"
-                : "text-[10px] top-[1.8rem] sm:top-[2.1rem]"
+                ? "text-[18px] right-[1.5rem] top-[2.4rem] sm:top-[2.8rem]"
+                : "text-[10px] right-[1rem] top-[2.2rem] sm:top-[2.5rem]"
             }  
-          right-[1.6rem] sm:right-[2rem] bg-gradient-to-tr from-purple-500 to-red-500 text-transparent bg-clip-text`}>
+            bg-gradient-to-tr from-purple-500 to-red-500 text-transparent bg-clip-text`}>
             {user ? (
               <span>Hi {user.firstName}!</span>
             ) : (
               <span>
-                Hi, I'm Jenna!
+                Hi, I'm the Wizard!
                 <br />
-                The rules AI.
-                <br />
-                Ready to help!
+                How can I help?!
               </span>
             )}
           </span>
@@ -69,9 +45,7 @@ export default function Home() {
           Get Started Now
         </Link>
       </section>
-
       <Features />
-
       <Pricing />
     </>
   );
