0d7145befb25fe171eb2f152af1cd12d3587a217
laith <laithalwani@gmail.com>
2025-01-25 00:05:55 -0500

    changes vectore search to 5 instead of 10, project is in Alpha

--- app/page.js
+++ app/page.js
@@ -17,12 +17,14 @@ export default function Home() {
         <h2 className="text-4xl mb-2 inline-block font-extrabold bg-gradient-to-r from-indigo-500  to-indigo-600 dark:from-yellow-400 dark:to-yellow-500 text-transparent bg-clip-text">
           Meepletron
         </h2>
-        <p className="italic text-md font-semibold ">Your Ultimate Board Game Companion</p>
+        <h3 className="text-3xl text-red-500 mt-4 font-semibold animate-bounce uppercase">Alpha</h3>
+        <small className="text-sm mb-4">This project is stil in alpha testing.</small>
         <p className="text-lg mt-4">
           Meepletron is your AI-powered board game expert, answering questions directly from
           manuals. Say goodbye to wasting time on game night searching for rule details—get instant
           answers and keep the fun going!
         </p>
+        <p className="italic text-md font-semibold ">Your Ultimate Board Game Companion</p>
         <Link
           href="/boardgames"
           className="mt-6 inline-block bg-indigo-600 text-white py-3 px-6 rounded-lg shadow hover:bg-indigo-700 dark:bg-yellow-500 dark:hover:bg-yellow-400 dark:text-slate-900 font-bold">
