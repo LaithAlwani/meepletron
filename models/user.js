24cd1125343055778c04f3d2d48b7ea6f9131e32
laith <laithalwani@gmail.com>
2025-01-25 23:20:48 -0500

    edits model

--- models/user.js
+++ models/user.js
@@ -4,10 +4,11 @@ const UserSchema = new Schema({
   firstname: String,
   lastname: Strting,
   email: String,
-  avatar: String, 
+  avatar: String,
+  visitedLast: [{ type: mongoose.Schema.Types.ObjectId, ref: "Boardgame" }],
   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Boardgame" }],
 });
 
-const User = models.User || model("User", UserSchema)
+const User = models.User || model("User", UserSchema);
 
 export default User;
