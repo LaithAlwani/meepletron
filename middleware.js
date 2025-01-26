d9e7406d9f748ab55555f566b582419b2e893b1d
laith <laithalwani@gmail.com>
2025-01-25 23:20:35 -0500

    changes redirect

--- middleware.js
+++ middleware.js
@@ -1,9 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([]);
 
 export default clerkMiddleware(async(auth,req)=>{
-  if(isProtectedRoute(req)) await auth.protect()
+  if (!auth().userId && isProtectedRoute(req)) return NextResponse.redirect(new URL('/sign-in', req.url));
 });
 
 export const config = {
