4c22008153b2efeb8b5079fc6ed2f1668f7b4c80
laith <laithalwani@gmail.com>
2025-02-04 16:11:09 -0500

    updates username

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ import { model, models, Schema } from "mongoose";
 const UserSchema = new Schema({
   first_name: { type: String, required: true },
   last_name: { type: String },
-  username: { type: String, required: true },
+  username: { type: String},
   email_address: { type: String,  required: true },
   avatar: String,
   last_visited: [{ type: Schema.Types.ObjectId, ref: "Boardgame" }],
