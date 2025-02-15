5a0fcf2abde7d1ef7d630dbb0155b0fd7cf6b650
laith <laithalwani@gmail.com>
2025-02-15 13:14:40 -0500

    adds aria label to links

--- components/boardgame/BoardgameContainer.jsx
+++ components/boardgame/BoardgameContainer.jsx
@@ -7,7 +7,7 @@ export default function BoardgameContainer({ boardgame }) {
   const { _id, image, title, parent_id } = boardgame;
   return (
     <div className="relative">
-      <Link href={`/boardgames/${_id}`} className="block w-[11rem] h-[11rem]">
+      <Link href={`/boardgames/${_id}`} className="block w-[11rem] h-[11rem]" aria-label={`${boardgame.title}`}>
         <Image
           src={image}
           alt={title}
@@ -22,6 +22,7 @@ export default function BoardgameContainer({ boardgame }) {
       </span> */}
       <Link
         href={`/boardgames/${parent_id || _id}/chat`}
+        aria-label={`${boardgame.title} chat`}
         className="absolute bottom-1 right-1 text-white bg-blue-600 dark:bg-yellow-500 dark:text-slate-900 rounded-full p-2">
         <BsChatDots size={22} />
       </Link>
