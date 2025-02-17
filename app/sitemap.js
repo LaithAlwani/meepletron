c4d09fe254672e137e7800d4f0363b4d10835c75
laith <laithalwani@gmail.com>
2025-02-17 17:33:11 -0500

    updates sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -5,23 +5,14 @@ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 export default async function sitemap() {
   const allboardgames = await getBoardgames();
 
-  const boardgamePages = allboardgames.flatMap((bg) => {
-    const gameSlug = `${bg.slug || bg._id}`; // Use slug if available, fallback to ID
-
-    return [
-      {
-        url: `${baseUrl}/boardgames/${gameSlug}`,
-        lastModified: bg.updatedAt,
-        changeFrequency: "yearly",
-        priority: 0.7,
-      },
-      {
-        url: `${baseUrl}/boardgames/${gameSlug}/chat`,
-        lastModified: bg.updatedAt,
-        changeFrequency: "yearly",
-        priority: 0.7,
-      },
-    ];
+  const boardgamePages = allboardgames.map((bg) => {
+    const gameSlug = `${bg.slug || bg._id}`;
+    return {
+      url: `${baseUrl}/boardgames/${gameSlug}`,
+      lastModified: bg.updatedAt,
+      changeFrequency: "yearly",
+      priority: 0.7,
+    };
   });
   return [
     {
