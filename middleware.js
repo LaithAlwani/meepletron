ed70a09ce81187a59aafc778deb7a51f4b72f9ff
laith <laithalwani@gmail.com>
2025-02-19 10:01:16 -0500

    adds admin route protection:

--- middleware.js
+++ middleware.js
@@ -1,17 +1,40 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+const isPublicRoute = createRouteMatcher([]);
 
-const isProtectedRoute = createRouteMatcher(["/admin(.*)",]);
+export default clerkMiddleware(async (auth, req) => {
+  // Check if the route is protected
+  const { userId, sessionClaims, redirectToSignIn } = await auth();
+  if (isPublicRoute(req)) {
+    //if user not autheticated send to signin page
+    if (!userId && !isPublicRoute(req)) return redirectToSignIn({ returnBackUrl: req.url });
+  }
 
-export default clerkMiddleware(async(auth,req)=>{
-  if (!auth().userId && isProtectedRoute(req)) await auth.protect();
+  if (isAdminRoute(req)) {
+    // Check if the user is authenticated
+    if (!userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    // Access the public metadata
+    const publicMetadata = sessionClaims?.metadata;
+
+    // Check if the user's role is 'admin'
+    if (publicMetadata.role !== "admin") {
+      return new Response("Forbidden: Admin access required", { status: 403 });
+    }
+  }
+  // If the user is authenticated and has the 'admin' role, proceed
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
