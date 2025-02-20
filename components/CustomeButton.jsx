d433297871a5e16176458e3e960eb94f6ecccd6d
laith <laithalwani@gmail.com>
2025-02-20 12:44:01 -0500

    adjusts button paddings

--- components/CustomeButton.jsx
+++ components/CustomeButton.jsx
@@ -4,13 +4,13 @@ import { motion } from "motion/react";
 
 export default function CustomButton({ onClick, children, className = "", disabled }) {
   return (
-    <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-      <button
-        onClick={onClick}
-        disabled={disabled}
-        className={` inline-block bg-blue-600 text-white py-3 px-6 shadow hover:bg-blue-700 disabled:bg-gray-500 dark:bg-yellow-500 dark:hover:bg-yellow-400 dark:text-slate-900 font-bold transition disabled:cursor-not-allowed ${className}`}>
-        {children}
-      </button>
-    </motion.div>
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      onClick={onClick}
+      disabled={disabled}
+      className={` inline-block bg-blue-600 text-white  p-3 shadow hover:bg-blue-700 disabled:bg-gray-500 dark:bg-yellow-500 dark:hover:bg-yellow-400 dark:text-slate-900 font-bold transition cursor-pointer disabled:cursor-not-allowed ${className}`}>
+      {children}
+    </motion.button>
   );
 }
