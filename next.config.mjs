8158b51d411fd4a7a8b05a307bcb660f83bf74d4
laith <laithalwani@gmail.com>
2025-02-20 14:58:29 -0500

    address swc warning

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 import withPWA from "next-pwa";
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   }, // Enable SWC minification for improved performance
