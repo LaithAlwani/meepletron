5bc0c45065a21e927c8beb188ffad4783ad666ef
laith <laithalwani@gmail.com>
2025-03-03 14:04:32 -0500

    adds only needed meta data to vector embeddings

--- app/api/boardgames/extract/route.js
+++ app/api/boardgames/extract/route.js
@@ -1,6 +1,7 @@
 import { getChunkedDocsFromPDF } from "@/lib/pdf-loader";
 import path from "path";
 import { NextResponse } from "next/server";
+import { metadata } from "@/app/layout";
 
 export async function POST(req) {
   const data = await req.json();
@@ -8,11 +9,25 @@ export async function POST(req) {
 
   if (!url) return NextResponse.json({ data: "File path or url missing" }, { status: 500 });
   try {
-    const chunks = await getChunkedDocsFromPDF(url);
-
+    const tempChunks = await getChunkedDocsFromPDF(url);
+    const chunks = cleanDocuments(tempChunks);
+    
     return NextResponse.json({ data: chunks, message: "Data Extracted" }, { status: 200 });
   } catch (err) {
     console.log(err);
     return NextResponse.json({ message: "Failed to upload" }, { status: 500 });
   }
+}
+
+
+function cleanDocuments(documents) {
+  return documents.map(({ pageContent, metadata }) => ({
+    pageContent,
+    metadata: {
+      source: metadata.source,
+      blobType: metadata.blobType,
+      totalPages: metadata.pdf.totalPages, // Move totalPages up
+      loc: metadata.loc
+    }
+  }));
 }
