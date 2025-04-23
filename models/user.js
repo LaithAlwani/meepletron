3fbdb1e78448194e7047332bfff53f2be15c5be3
laith <laithalwani@gmail.com>
2025-04-23 08:51:25 -0400

    makes first name optional

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ import { model, models, Schema } from "mongoose";
 
 const UserSchema = new Schema(
   {
-    first_name: { type: String, required: true },
+    first_name: { type: String },
     last_name: { type: String },
     username: { type: String },
     email_address: { type: String, required: true },
